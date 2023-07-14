c1f369de327bcd0c300b61488b650d856a6b464d
李兴广 <lixingguang@xunlei.com>
2023-07-14 13:49:24 +0800

    feat(coc): 支持切换显示

--- coc-extension/index.js
+++ coc-extension/index.js
@@ -1,11 +1,14 @@
-const { diagnosticManager, workspace, Disposable } = require('coc.nvim')
+const { diagnosticManager, workspace, Disposable, commands } = require('coc.nvim')
 
 exports.activate = async context => {
-  const { nvim } = workspace
+  const { nvim, getConfiguration } = workspace
   const NS = await nvim.createNamespace('coc-lsp-lines')
   const { subscriptions } = context
+  const configuration = getConfiguration('lsp-lines');
+  let enabled =configuration.get('enabled', true)
   subscriptions.push(
     diagnosticManager.onDidRefresh(e => {
+      if (!enabled) return
       nvim.call('luaeval', [
         "require'lsp_lines.render'.show(_A[1], _A[2], _A[3], _A[4], _A[5])",
         [
@@ -28,6 +31,18 @@ exports.activate = async context => {
           vim.api.nvim_buf_clear_namespace(bufnr, ${NS}, 0, -1)
         end
         `)
+    }),
+    commands.registerCommand('lsp_lines.toggle', () => {
+      if (enabled) {
+        enabled = false
+        nvim.lua(`
+          for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+            vim.api.nvim_buf_clear_namespace(bufnr, ${NS}, 0, -1)
+          end
+          `)
+      } else {
+        enabled = true
+      }
     })
   )
 }
